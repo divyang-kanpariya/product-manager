ca5dff39d148e613f6e104414baa0313f176c1e4
Your Name <you@example.com>
2023-02-09 19:26:25 +0530

    h

--- src/components/InputDetails.jsx
+++ src/components/InputDetails.jsx
@@ -9,19 +9,19 @@ import { Category } from '@mui/icons-material';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import Footer from './Footer';
+import { type } from '@testing-library/user-event/dist/type';
 
 const InputDetails = () => {
 
   const [category, setCategory] = useState("");
-  const [image, setImage] = useState([]);
+  const [image, setImage] = useState({ id: "", name: "", category: "", discription: "" });
 
-  const [state, dispatch] = useReducer(reducer ,{ id: "", name: "", category: "", discription: "" });
-console.log(state);
+  const [state, dispatch] = useReducer(reducer, { id: "", name: "", category: "", discription: "" });
+
   const products = [];
 
-  function reducer(state,action){
 
-  }
+
 
   const form = {
     display: 'flex',
@@ -36,7 +36,7 @@ console.log(state);
     setCategory(selectedCategory);
   }
 
-  function addProduct(event){
+  function addProduct(event) {
     console.log(event);
   }
 
@@ -55,34 +55,36 @@ console.log(state);
     <>
       <Header />
       <div className='w-40 justify-content-center col-12 d-flex'>
-        <table className='text-center m-auto '>
-          <tr><td colSpan={2}><p className='fs-2 text-primary'>Enter Product Details</p></td></tr>
-          <tr>
-            <td className='fs-5 text-primary px-3'>Name</td>
-            <td> <TextField autoFocus='true' sx={form} label='Name' /></td>
-          </tr>
-          <tr>
-            <td className='fs-5 text-primary px-3'>Category</td>
-            <td fullHight > <FormControl sx={form}>
-              <InputLabel>Category</InputLabel>
-              <Select value={category} onChange={categorySelector} label="category">
-                {categories.map((name) => (
-                  <MenuItem key={name} value={name}
-                  >{name}
-                  </MenuItem>))}
-              </Select>
-            </FormControl></td>
-          </tr>
-          <tr>
-            <td><td className='fs-5 text-primary px-3'>Images</td></td>
-            <td><input type="file" onChange={uploadImages} /></td>
-          </tr>
+        <form>
+          <table className='text-center m-auto '>
+            <tr><td colSpan={2}><p className='fs-2 text-primary'>Enter Product Details</p></td></tr>
+            <tr>
+              <td className='fs-5 text-primary px-3'>Name</td>
+              <td> <TextField onChange={} autoFocus='true' sx={form} label='Name' /></td>
+            </tr>
+            <tr>
+              <td className='fs-5 text-primary px-3'>Category</td>
+              <td fullHight > <FormControl sx={form}>
+                <InputLabel>Category</InputLabel>
+                <Select value={category} onChange={} label="category">
+                  {categories.map((name) => (
+                    <MenuItem key={name} value={name}
+                    >{name}
+                    </MenuItem>))}
+                </Select>
+              </FormControl></td>
+            </tr>
+            <tr>
+              <td><td className='fs-5 text-primary px-3'>Images</td></td>
+              <td><input type="file" onChange={uploadImages} /></td>
+            </tr>
 
-          <tr>
-            <td className='fs-5 text-primary px-3 '>Discription</td>
-            <td><TextField multiline={true} minRows={3} sx={form} label='' placeholder='Enter More Information about your product....' /></td></tr>
-          <tr><td colSpan={2}><button class="btn btn-primary mt-5 fs-5 " type="submit" onClick={addProduct}>Add Product</button></td></tr>
-        </table>
+            <tr>
+              <td className='fs-5 text-primary px-3 '>Discription</td>
+              <td><TextField onChange={} multiline={true} minRows={3} sx={form} label='' placeholder='Enter More Information about your product....' /></td></tr>
+            <tr><td colSpan={2}><button class="btn btn-primary mt-5 fs-5 " type="submit" onClick={addProduct}>Add Product</button></td></tr>
+          </table>
+        </form>
 
 
 
